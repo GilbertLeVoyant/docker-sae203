24f01c155027b5c3f7cdc0ed7b64d545dd6b18f8
Hazet Alex <alex.hazet@gmail.com>
2025-04-29 09:42:12 +0200

    Ajout Scrap

--- java/Scrap.java
+++ java/Scrap.java
@@ -1,6 +1,18 @@
-import org.jsoup.*;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
 public class Scrap
 {
-	public Scrap
+	public static void main(String[] args) 
+	{
+		try {
+			String url = "http://di-docker:54688/";
+			Document doc = Jsoup.connect(url).get();
+			System.out.println("Title: " + doc.title());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("Scraping completed.");
+		System.exit(0);
+	}
 }
