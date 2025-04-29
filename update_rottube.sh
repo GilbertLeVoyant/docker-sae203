241ab02383d7329920b0b0bceba2feb90e0d730d
GilbertLeVoyant <emilienfoyer@gmail.com>
2025-04-29 13:50:47 +0200

    Amélioration script update

--- update_rottube.sh
+++ update_rottube.sh
@@ -1,18 +1,39 @@
 #!/bin/bash
 
-# Arrêter le conteneur nommé "rottube" s'il est en cours d'exécution
-docker stop rottube
+# Fonction pour afficher une animation de chargement
+loading_animation() {
+  local pid=$1
+  local delay=0.1
+  local spinstr='|/-\'
+  echo -n " "
+  while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+    local temp=${spinstr#?}
+    printf " [%c]  " "$spinstr"
+    spinstr=$temp${spinstr%"$temp"}
+    sleep $delay
+    printf "\b\b\b\b\b\b"
+  done
+  echo "    "
+}
 
-# Supprimer le conteneur nommé "rottube" s'il existe
-docker rm rottube
+echo "Arrêt du conteneur 'rottube' s'il est en cours d'exécution..."
+docker stop rottube &> /dev/null &
+loading_animation $!
 
-# Mettre à jour le code source depuis le dépôt Git
-git pull
+echo "Suppression du conteneur 'rottube' s'il existe..."
+docker rm rottube &> /dev/null &
+loading_animation $!
 
-# Construire une nouvelle image Docker avec le tag "rottube"
-docker build -t rottube .
+echo "Mise à jour du code source depuis le dépôt Git..."
+git pull &> /dev/null &
+loading_animation $!
 
-# Lancer un nouveau conteneur avec le nom "rottube" et exposer le port 54688
-docker run -d -p 54688:80  --name rottube rottube
+echo "Construction de la nouvelle image Docker 'rottube'..."
+docker build -t rottube . &> /dev/null &
+loading_animation $!
+
+echo "Lancement du nouveau conteneur 'rottube'..."
+docker run -d -p 54688:80 --name rottube rottube &> /dev/null &
+loading_animation $!
 
 echo "Le conteneur 'rottube' a été mis à jour et relancé avec succès."
